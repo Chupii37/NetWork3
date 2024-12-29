0cbcc516adf4bd17f3211006282a0e20fcfd66d2
Chupii <162031546+Chupii37@users.noreply.github.com>
2024-12-29 18:34:42 +0700

    NetWork3.sh

--- NetWork3.sh
+++ NetWork3.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Update and install Docker
-sudo apt update
-sudo apt install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
+# Update and upgrade the system packages
+sudo apt update && sudo apt upgrade -y
+
+# Update Docker (if needed)
+sudo apt install --only-upgrade docker.io
+
+# Restart Docker service to apply updates
+sudo systemctl restart docker
 
 # Create directory if it doesn't exist
 mkdir -p /home/$USER/network3-docker
@@ -21,7 +24,7 @@ fi
 cat <<EOL > Dockerfile
 FROM ubuntu:latest
 RUN apt-get update && apt-get install -y wget ufw tar nano sudo net-tools iproute2 procps
-RUN wget https://network3.io/ubuntu-node-v2.1.0.tar && tar -xf ubuntu-node-v2.1.0.tar && rm ubuntu-node-v2.1.0.tar
+RUN wget https://network3.io/ubuntu-node-v2.1.1.tar.gz && tar -xzf ubuntu-node-v2.1.1.tar.gz && rm ubuntu-node-v2.1.1.tar.gz
 WORKDIR /ubuntu-node
 EXPOSE 8080
 CMD ["bash", "-c", "bash manager.sh up; bash manager.sh key; exec bash"]
@@ -34,6 +37,9 @@ docker build -t network3-docker-image .
 # Set container name and port number (default values)
 container_name="network3-docker-container"
 port_number=8080
+
+# Remove the old container if it exists
+docker rm -f $container_name
 
 # Run Docker container with necessary privileges
 echo "Starting the container with required privileges..."
